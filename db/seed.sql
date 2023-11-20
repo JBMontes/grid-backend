1f11c83670aaa613b9cd821d45575dac318c8fd5
Jeremy Montes <jeremymontes@pursuit.org>
2023-11-20 09:35:47 -0500

    deployed to web

--- db/seed.sql
+++ db/seed.sql
@@ -3,4 +3,4 @@
 INSERT INTO grid (title, priority,description,deadline, completed,category) VALUES 
 ('Class Project', 3, 'Tuner App', '2023-11-28', true, 'school'),
 ('Shape Up', 1, 'barber shop appointment', '2023-10-10', false, 'personal'),
-('Date Night', 4,'Date with Shorty', '2023-4-3', false, 'personal')
+('Date Night', 4,'Date with Shorty', '2023-4-3', false, 'personal');
